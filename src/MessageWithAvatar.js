510a816b56df1e7f37e188efe319863adf71788d
Rohit Yadav <rohit@turbot.com>
2019-06-30 10:38:33 +0530

    Adding useDocWithCache function for getting cached user

--- src/MessageWithAvatar.js
+++ src/MessageWithAvatar.js
@@ -1,38 +1,9 @@
-import React, { useState, useEffect } from "react";
-import { db } from "./firebase";
+import React from "react";
+import useDocWithCache from "../src/utils/useDocWithCache";
 
-const cache = {};
-const pendingCache = {};
-
-function useDoc(path) {
-  const [doc, setDoc] = useState(cache[path]);
-
-  useEffect(() => {
-    if (doc) {
-      return;
-    }
-    let stillMounted = true;
-    const pending = pendingCache[path];
-    const promise = pending || (pendingCache[path] = db.doc(path).get());
-
-    promise.then(doc => {
-      if (stillMounted) {
-        const user = {
-          ...doc.data(),
-          id: doc.id
-        };
-        setDoc(user);
-        cache[path] = user;
-      }
-    });
-    return () => {
-      stillMounted = false;
-    };
-  }, [path, doc]);
-  return doc;
-}
 export default function MessageWithAvatar({ message, showDay }) {
-  const author = useDoc(message.user.path);
+  const author = useDocWithCache(message.user.path);
+
   return (
     <div key={message.id}>
       {showDay && (
