9ee086d2950e46245378e42306bbf5ffb6e155f7
Rohit Yadav <rohit@turbot.com>
2019-06-30 21:17:54 +0530

    Optimize the sync code

--- functions/index.js
+++ functions/index.js
@@ -1,23 +1,4 @@
-const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 
 admin.initializeApp();
-
-const db = admin.firestore();
-
-exports.onUserSatausChanged = functions.database
-  .ref("/status/{userId}")
-  .onUpdate((change, context) => {
-    const eventStatus = change.after.val();
-    const userDoc = db.doc(`user/${context.params.userId}`);
-    return change.after.ref.once("value").then(snapshot => {
-      const status = snapshot.val();
-      if (status.lastChanged > eventStatus.lastChanged) {
-        return;
-      }
-      eventStatus.lastChanged = new Date(eventStatus.lastChanged);
-      userDoc.update({
-        status: eventStatus
-      });
-    });
-  });
+exports.onUserSatausChanged = require("./triggers/onUserSatausChanged");
