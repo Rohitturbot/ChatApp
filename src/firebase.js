256f3b9decc3118859d498743b3b8d0ad2d103e1
Rohit Yadav <rohit@turbot.com>
2019-07-01 12:01:46 +0530

    Removing access keys

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import "firebase/firestore";
 import "firebase/auth";
 
 var firebaseConfig = {
-  apiKey: "AIzaSyBgb5xpNRKIfAhI1L3PHF-RI8gfuXmGU70",
-  authDomain: "chat-app-d8e91.firebaseapp.com",
-  databaseURL: "https://chat-app-d8e91.firebaseio.com",
-  projectId: "chat-app-d8e91",
-  storageBucket: "chat-app-d8e91.appspot.com",
-  messagingSenderId: "697216982746",
-  appId: "1:697216982746:web:12c3307a418b347f"
+  apiKey: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+  authDomain: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+  databaseURL: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+  projectId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+  storageBucket: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+  messagingSenderId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+  appId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
